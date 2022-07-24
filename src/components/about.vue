f2ba5e352d693547c0a8fc4745e0974f92e782b5
Soyeon Park <psy_sott120@naver.com>
2022-07-25 01:18:54 +0900

    svg 오류

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="hello">
     <div class="about_title">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1009.586 223.199">
-      <text x="0" y="0">
-        <tspan dy="0.92em" x="0">ABOUT</tspan>
-      </text>
-    </svg>
+    <!-- <img src="../assets/about.svg" alt=""> -->
+    <div class="main_visual_typo_strok">
+      <span class="main_visual_typo_fake">ABOUT</span>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1009.586 223.199">
+          <text x="0" y="0"><tspan dy="0.92em" x="0">ABOUT</tspan></text>
+      </svg>
+    </div>
     <h1>ME.</h1>
     </div>
     <div class="my_img">      
@@ -53,5 +55,46 @@ h1{
   width: 100%;
   height: 474px;
   background-color: beige;
+}
+
+.main_visual_typo_strok{
+      display: inline-block;
+    vertical-align: middle;
+    margin-right: 38px;
+    position: relative;
+}
+
+.main_visual_typo{
+  font-size: 183px;
+}
+
+.main_visual_typo_fake{
+  color: transparent;
+    opacity: 0;
+}
+
+.main_visual_typo_strok svg {
+    display: block;
+    width: 100%;
+    height: auto;
+    position: absolute;
+    top: 50%;
+    left: 0;
+    -webkit-transform: translateY(-50%);
+    -ms-transform: translateY(-50%);
+    transform: translateY(-50%);
+}
+
+.main_visual_typo_strok text {
+    paint-order: stroke;
+    stroke: #1e39b4;
+    stroke-width: 2px;
+    stroke-linecap: butt;
+    stroke-linejoin: miter;
+    fill: rgba(255, 255, 255, 0);
+}
+
+.main_visual_typo_strok text tspan{
+      font-size: 183px;
 }
 </style>
