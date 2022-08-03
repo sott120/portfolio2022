5cbbc47a2876b885d1dcc181173aad677011128b
Soyeon Park <psy_sott120@naver.com>
2022-08-04 02:24:39 +0900

    gsap 모듈추가

--- src/main.js
+++ src/main.js
@@ -4,4 +4,14 @@ import App from './App.vue'
 import './static/css/reset.css'
 import './static/css/common.css'
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+// No need to export ScrollToPlugin or TextPlugin
+// as you probably won't need to reference them directly
+export { gsap, ScrollTrigger };
+
+
 createApp(App).mount('#app')
