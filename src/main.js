46998361375c4481593bfe73988db2e80abb277f
sott120 <psy_sott120@naver.com>
2022-08-08 13:12:49 +0900

    firebase 추가했지만 에러

--- src/main.js
+++ src/main.js
@@ -14,4 +14,25 @@ gsap.registerPlugin(ScrollTrigger);
 export { gsap, ScrollTrigger };
 
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyApNx7mfe6is0Cg6Wp9B61ZcZ__feq6gQc",
+  authDomain: "sott120-portfolio.firebaseapp.com",
+  databaseURL: "https://sott120-portfolio-default-rtdb.firebaseio.com",
+  projectId: "sott120-portfolio",
+  storageBucket: "sott120-portfolio.appspot.com",
+  messagingSenderId: "748811021419",
+  appId: "1:748811021419:web:b4d89a3668bf1cadeb952e"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+
 createApp(App).mount('#app')
